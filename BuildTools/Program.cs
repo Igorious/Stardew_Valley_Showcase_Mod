5a54a15851d5b2e8c6533aa38e0d2e4c5587b89e
Igorious <igorious92@gmail.com>
2017-01-16 00:21:01 +0300

    Removed log file from build tool.

--- BuildTools/Program.cs
+++ BuildTools/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Globalization;
+﻿using System.Globalization;
 using System.IO;
 using System.Reflection;
 using System.Runtime.InteropServices;
@@ -11,29 +10,21 @@ namespace Igorious.BuildManifest
     {
         public static void Main(string[] args)
         {
-            try
-            {
-                var assemblyPath = args[0];
-                var assembly = Assembly.LoadFrom(assemblyPath);
+            var assemblyPath = args[0];
+            var assembly = Assembly.LoadFrom(assemblyPath);
 
-                var manifest = new Manifest
-                {
-                    Name = assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title,
-                    Author = assembly.GetCustomAttribute<AssemblyCompanyAttribute>().Company,
-                    Version = GetVersion(assembly),
-                    Description = assembly.GetCustomAttribute<AssemblyDescriptionAttribute>().Description,
-                    UniqueID = assembly.GetCustomAttribute<GuidAttribute>().Value,
-                    EntryDll = Path.GetFileName(assemblyPath),
-                };
-
-                var json = JsonConvert.SerializeObject(manifest, new JsonSerializerSettings { Formatting = Formatting.Indented });
-                File.WriteAllText(Path.Combine(Path.GetDirectoryName(assemblyPath), "manifest.json"), json);
-            }
-            catch (Exception e)
+            var manifest = new Manifest
             {
-                File.WriteAllText(@"D:\Temp.log", e.ToString());
-                throw;
-            }
+                Name = assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title,
+                Author = assembly.GetCustomAttribute<AssemblyCompanyAttribute>().Company,
+                Version = GetVersion(assembly),
+                Description = assembly.GetCustomAttribute<AssemblyDescriptionAttribute>().Description,
+                UniqueID = assembly.GetCustomAttribute<GuidAttribute>().Value,
+                EntryDll = Path.GetFileName(assemblyPath),
+            };
+
+            var json = JsonConvert.SerializeObject(manifest, new JsonSerializerSettings { Formatting = Formatting.Indented });
+            File.WriteAllText(Path.Combine(Path.GetDirectoryName(assemblyPath), "manifest.json"), json);
         }
 
         private static ModVersion GetVersion(Assembly assembly)
